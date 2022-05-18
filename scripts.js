300cbce056d3c3fbd54125fe9b4afa0ac755e2d3
Marlyn_Mayienga <marlymayienga@gmail.com>
2022-05-18 15:02:49 +0300

    modified the mobile menu

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger-btn");
-const closeIcon= document.querySelector(".closeIcon");
+const hamburger = document.querySelector(".hamburger-btn");
+const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
@@ -17,3 +17,80 @@ function toggleMenu() {
 }
 menu.addEventListener("click", toggleMenu);
 hamburger.addEventListener("click", toggleMenu);
+
+const body = document.createElement("body");
+const section = document.createElement("work-item");
+body.appendChild(work - item);
+section.innerHTML = `
+<div class="modal-container">
+<img class="work-img" src="./project.jpg" alt="project image" />
+<div class="work-content">
+  <h4 class="work-item-title">Multi-Post Stories</h4>
+  <p class="project-section">
+    A daily selection of privately personalized reads; no accounts
+    or sign-ups required. has been the industry's standard dummy
+    text ever since the 1500s, when an unknown printer took a
+    standard dummy text.
+  </p>
+  <ul class="skillset">
+    <li>css</li>
+    <li>html</li>
+    <li>bootstrap</li>
+    <li>Ruby</li>
+  </ul>
+  <div class="work-item-actions">
+    <button class="button button-primary">
+      See Live <span><i class="fas fa-location-arrow"></i></span>
+    </button>
+    <button class="button button-primary">
+      See Source <span><i class="fab fa-github"></i></span>
+    </button>
+  </div>
+</div>
+</div>
+`;
+
+const open = document.queryselector(".See-project");
+open.addEventListener("click", () => {
+  const main = document.createElement("div");
+  main.className = 'main';
+  const popup = document.createElement("div");
+  popup.className = 'popup';
+  popup.innerHTML = `
+      <div class="modal-container">
+<img class="work-img" src="./project.jpg" alt="project image" />
+<div class="work-content">
+  <h4 class="work-item-title">Multi-Post Stories</h4>
+  <p class="project-section">
+    A daily selection of privately personalized reads; no accounts
+    or sign-ups required. has been the industry's standard dummy
+    text ever since the 1500s, when an unknown printer took a
+    standard dummy text.
+  </p>
+  <ul class="skillset">
+    <li>css</li>
+    <li>html</li>
+    <li>bootstrap</li>
+    <li>Ruby</li>
+  </ul>
+  <div class="work-item-actions">
+    <button class="button button-primary">
+      See Live <span><i class="fas fa-location-arrow"></i></span>
+    </button>
+    <button class="button button-primary">
+      See Source <span><i class="fab fa-github"></i></span>
+    </button>
+  </div>
+</div>
+</div>
+      `;
+
+
+      main.appendChild(popup);
+      body.appendChild(main);
+
+      const close = document.querySelector('');
+      close.addEventListener('click', ()=>{
+        body.removeChild(main);
+      })
+})
